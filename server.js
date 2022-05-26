0d4cd36d7b95484afb9b7e3d61f0670681036558
z3phyron <z3phyrondevs@gmail.com>
2022-05-26 17:35:28 +0100

    ..

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api/secret", require("./routes/secret"));
 
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) =>
